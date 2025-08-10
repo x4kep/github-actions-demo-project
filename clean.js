0c834bc06ed5b998f78d47d236eef0d6014bd858
Dusan Veselinovic <dusan.veselinovic.dev@gmail.com>
2025-08-10 20:15:21 +0200

    ZZZ

--- clean.js
+++ clean.js
@@ -1,18 +1,5 @@
-import rimraf from 'rimraf';
-import mkdirp from 'mkdirp';
+const rimraf = require('rimraf');
+const mkdirp = require('mkdirp');
 
-rimraf('./dist', (err) => {
-  if (err) {
-    console.error('Error deleting dist:', err);
-    process.exit(1);
-  } else {
-    mkdirp('./dist/logs')
-      .then(() => {
-        console.log('Cleaned ./dist and created ./dist/logs');
-      })
-      .catch((err) => {
-        console.error('Error creating dist/logs:', err);
-        process.exit(1);
-      });
-  }
-});
+rimraf.sync('./dist');
+mkdirp.sync('./dist/logs');
